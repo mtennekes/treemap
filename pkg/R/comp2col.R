37ef29f040fbe974f6cae530a12496fd0d8dd0bf
Martijn Tennekes <mtennekes@gmail.com>
2012-03-23 14:46:19 +0100

    improved comp2col

--- pkg/R/comp2col.R
+++ pkg/R/comp2col.R
@@ -1,12 +1,45 @@
 comp2col <-
 function(dat, showScale, palette) {
 	color <- colorRampPalette(palette,space="rgb")(99)
-
-	perc <-((dat$value - dat$value2)/dat$value2)*100
-	prettyP <- pretty(perc,n=8)
+	
+	# calculate percentages
+	# for legend
+	perc <-((dat$value - dat$value2)/dat$value2) * 100
+	
+	# edit perc
+	perc[perc==Inf] <- max(900, c(perc[perc!=Inf]))
+	perc[perc==0] <- min(-90, c(perc[perc!=0]))
+	
+	
+	perc2times <- function(p) {
+		ifelse(p>=0, 
+			   p/100,
+			   -(1/(1-(abs(p)/100)) - 1))
+	}
+	
+	times2log <- function(t) {
+		res <- numeric(length(t))
+		res[t>=0] <- log1p(t[t>=0])
+		res[t<0] <- -log1p(-t[t<0])
+		return(res)
+	}
+#browser()
+	
+	times <- times2log(perc2times(perc))
+	range_times <- range(times)
+	
+	if (range_times[1] >= 0 || range_times[2] <= 0) {
+		prettyP <-pretty(perc, n=8)
+	} else {
+		ratio <- abs(range_times) / sum(abs(range_times))
+		prettyP_neg <- pretty(c(0, perc[perc<0]), n=9*ratio[1])
+		prettyP_pos <- pretty(c(0, perc[perc>0]), n=9*ratio[2])
+		prettyP <- c(prettyP_neg, prettyP_pos[-1])
+	}
+	
 	n <- length(prettyP)	
-	maxPerc <- max(abs(prettyP))
-	scale <- round(((perc/maxPerc) *49)+50)
+	maxTimes <- max(abs(times))
+	scale <- round(((times/maxTimes) *49)+50)
 	
 	lineNpc <- convertHeight(unit(0.8,"lines"), "npc", valueOnly = TRUE)
 
@@ -16,7 +49,10 @@ function(dat, showScale, palette) {
 		legY <- rep(lineNpc,n)
 	 	legW <- rep(1/n,n)
 	 	legH <- rep(0.2,n)
-		legScale <- round(((prettyP/maxPerc) *49)+50)
+		prettyT <- times2log(perc2times(prettyP))
+		legScale <- round(((prettyT/maxTimes) *49)+50)
+		legScale[legScale<1] <- 1
+		legScale[legScale>99] <- 99
 	 	legCol <- color[legScale]
 	 	leg <- data.frame(X=legX,Y=legY,W=legW,H=legH,Col=legCol)
 		
--- treemap_examples.R
+++ treemap_examples.R
@@ -9,7 +9,6 @@ tmPlot(sbsData,
 	   vColor="employees08",
 	   type="index")
 
-load("ps0607_final.Rdata")
 
 
 
@@ -19,7 +18,8 @@ rec <- c(0,0, 6, 4)
 squarified(value, rec)
 
 
-# data editing workshop
+## load final ps 06 and 07 data, apply weights and merge them
+load("ps0607_final.Rdata")
 
 ps06final[,8:22] <- lapply(ps06final[,8:22], FUN="*", ps06final$weight)
 ps06final$weight <- NULL
@@ -28,14 +28,30 @@ ps07final[,11:25] <- lapply(ps07final[,11:25], FUN="*", ps07final$weight)
 ps07final$weight <- NULL
 
 
-ps6f <- aggregate(ps06final[,7:21], by=list(sector=ps06final$sector, subsector=ps06final$subsector, SBI3d=ps06final$SBI3d), FUN=function(x)sum(as.numeric(x), na.rm=TRUE))
+ps6f <- aggregate(ps06final[,7:21], by=list(sector=ps06final$sector, subsector=ps06final$subsector, SBI3d=ps06final$SBI3d, SBI=ps06final$SBI), FUN=function(x)sum(as.numeric(x), na.rm=TRUE))
 
-ps7f <- aggregate(ps07final[,10:24], by=list(sector=ps07final$sector, subsector=ps07final$subsector, SBI3d=ps07final$SBI3d), FUN=function(x)sum(as.numeric(x), na.rm=TRUE))
+ps7f <- aggregate(ps07final[,10:24], by=list(sector=ps07final$sector, subsector=ps07final$subsector, SBI3d=ps07final$SBI3d, SBI=ps07final$SBI), FUN=function(x)sum(as.numeric(x), na.rm=TRUE))
 
-psfinal <- merge(ps6f, ps7f, by=c("sector", "subsector", "SBI3d"),suffixes = c(".2006",".2007"))
+psfinal <- merge(ps6f, ps7f, by=c("sector", "subsector", "SBI3d", "SBI"),suffixes = c(".2006",".2007"))
 
-tmPlot(psfinal, vSize="value_added.2007", vColor="value_added.2006", index=c("sector", "subsector", "SBI3d"), type="comp", 
-	   fontsize.labels=8)
+## data editing workshop
+
+tmPlot(psfinal, vSize="value_added.2007", vColor="value_added.2006", index=c("sector", "subsector"), type="comp", 
+	   fontsize.labels=c(12,8))
+
+tmPlot(subset(psfinal,subset=sector=="Manufacturing"), vSize="value_added.2007", vColor="value_added.2006", index=c("subsector", "SBI3d"), type="comp", 
+	   fontsize.labels=c(12,10))
+
+
+tmPlot(subset(psfinal,subset=subsector=="Electrical and optical equipment"), vSize="value_added.2007", vColor="value_added.2006", index=c("SBI3d", "SBI"), type="comp", 
+	   fontsize.labels=c(12,10))
+
+
+tmPlot(psfinal, vSize="value_added.2007", vColor="value_added.2006", index=c("sector", "subsector", "SBI3d", "SBI"), type="comp", 
+	   fontsize.labels=c(12,10))
+
+
+
 
 
 tmPlot(psfinal, vSize="turnover.2007", vColor="employees.2007/1000", index=c("sector", "subsector", "SBI3d"), type="dens", 
