5a703c1d7fbe0d25bd61fec18a09675e7eb5631a
Martijn Tennekes <mtennekes@gmaill.com>
2013-08-24 17:49:08 +0200

    updated grid example in test

--- test/grid.R
+++ test/grid.R
@@ -1,34 +1,15 @@
-
-library(grid)
+require(grid)
 grid.newpage()
-grid.rect()
+
 data(business)
-do_treemap <- function(ind){
-    vp <- viewport(layout.pos.col=1, layout.pos.row=ind)
-    #pushViewport(vp)
-    treemap(business, index=c("NACE1"), vSize="turnover", type="index",vp=vp)
-    #popViewport()
-    #popViewport() #treemap doees not seem to pop corretly
-    #popViewport() #and one more!
+numVars <- c("turnover", "turnover.prev", "employees", "employees.prev")
+
+pushViewport(viewport(layout=grid.layout(2, 2, widths=c(0.4,0.6), heights=c(0.7,0.3))))
+
+for (i in 1:2) {
+    for (j in 1:2) {
+        vp <- viewport(layout.pos.col=i, layout.pos.row=j)
+        treemap(business, index=c("NACE1", "NACE2"), vSize=numVars[i+(j-1)*2],
+                type="index",vp=vp)
+    }
 }
-
-lapply(1:3, do_treemap)
-
-
-grid.newpage()
-grid.rect()
-pushViewport(viewport(layout=grid.layout(3, 1), name="3_1_grid"))
-
-vp <- viewport(layout.pos.col=1, layout.pos.row=1, name="row1")
-treemap(business, index=c("NACE1"), vSize="turnover", type="index",vp=vp)
-
-vp <- viewport(layout.pos.col=1, layout.pos.row=2)
-treemap(business, index=c("NACE1"), vSize="employees", type="index",vp=vp)
-
-vp <- viewport(layout.pos.col=1, layout.pos.row=3)
-treemap(business, index=c("NACE1"), vSize="turnover", type="index",vp=vp)
-
-
-
-
-tp <- treemap(business, index=c("NACE1", "NACE2", "NACE3"), vSize="turnover", type="index")
