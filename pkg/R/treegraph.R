c5d3ec8a63a74c6d33e82b825f60266d313a910a
Martijn Tennekes <mtennekes@gmail.com>
2014-01-08 13:56:50 +0100

    added graph layout argument

--- pkg/R/treegraph.R
+++ pkg/R/treegraph.R
@@ -7,6 +7,8 @@
 #' @param palette.HCL.options palette.HCL.options (see \code{\link{treemap}})
 #' @param show.labels show the labels
 #' @param rootlabel name of the rootlabel
+#' @param vertex.layout layout algorithm
+#' @param vertex.layout.params list of arguments passed to \code{vertex.layout}
 #' @param truncate.labels number of characters at which the levels are truncated. Either a single value for all index variables, or a vector of values for each index variable
 #' @param vertex.size vertex.size (see \code{\link[igraph:igraph.plotting]{igraph.plotting}})
 #' @param vertex.label.dist vertex.label.dist (see \code{\link[igraph:igraph.plotting]{igraph.plotting}})
@@ -24,13 +26,13 @@
 #' @import igraph
 #' @import colorspace
 #' @export
-treegraph <- function(dtf, index=names(dtf), palette.HCL.options, show.labels=FALSE, rootlabel="", truncate.labels=NULL, vertex.size=3, vertex.label.dist=0.3, vertex.label.cex=0.8, vertex.label.family="sans", vertex.label.color="black", ...) {
+treegraph <- function(dtf, index=names(dtf), palette.HCL.options, show.labels=FALSE, rootlabel="", vertex.layout, vertex.layout.params, truncate.labels=NULL, vertex.size=3, vertex.label.dist=0.3, vertex.label.cex=0.8, vertex.label.family="sans", vertex.label.color="black", ...) {
     palette.HCL.options <- tmSetHCLoptions(palette.HCL.options)
     
     k <- length(index)
     dat <- treepalette(dtf, index,
-                palette.HCL.options=palette.HCL.options,
-                return.parameters=TRUE)[, 1:(k+1)]
+                       palette.HCL.options=palette.HCL.options,
+                       return.parameters=TRUE)[, 1:(k+1)]
     
     dat <- unique(dat)
     
@@ -47,16 +49,16 @@ treegraph <- function(dtf, index=names(dtf), palette.HCL.options, show.labels=FA
     vdat <- unique(vdat)
     rootname <- dat$parent[which(substr(dat$parent, 1, 2)=="NA")][1]
     vdat <- rbind(list(current=rootname, color=hcl(h=0, c=0, l=palette.HCL.options$luminance-palette.HCL.options$luminance_slope)), vdat)
-
+    
     
     
     g <- graph.data.frame(dat[,c("current", "parent")], vertices=vdat, directed=FALSE)
     
     if (show.labels) {
-            
+        
         vdat_names <- gsub("__NA", "", vdat$current, fixed=TRUE)
         vdat_names[1] <- rootlabel
-
+        
         ssres <- strsplit(vdat_names, split="__", fixed=TRUE)
         ssres[[1]] <- rootlabel
         vdat_names <- sapply(ssres, function(x)x[length(x)])
@@ -74,7 +76,15 @@ treegraph <- function(dtf, index=names(dtf), palette.HCL.options, show.labels=FA
     
     
     par(mai=c(0,0,0,0))
-    plot(g, vertex.size=vertex.size, vertex.frame.color=NA, vertex.label=vdat_names, layout=
-             layout.reingold.tilford(g, circular=T, root=1), vertex.label.dist=vertex.label.dist, vertex.label.cex=vertex.label.cex, vertex.label.family=vertex.label.family, vertex.label.color=vertex.label.color, vertex.label.degree=-pi/2.5, ...)
+    if (missing(vertex.layout)){
+        vertex.layout=layout.reingold.tilford
+    }
+    if (missing(vertex.layout.params)){
+        vertex.layout.params = list(circular=T, root=1)
+    }
+    vertex.layout.params = c(list(graph=g), vertex.layout.params)
+    vertex.layout = do.call(vertex.layout, vertex.layout.params)
+    plot(g, vertex.size=vertex.size, vertex.frame.color=NA, vertex.label=vdat_names, layout=vertex.layout
+         , vertex.label.dist=vertex.label.dist, vertex.label.cex=vertex.label.cex, vertex.label.family=vertex.label.family, vertex.label.color=vertex.label.color, vertex.label.degree=-pi/2.5, ...)
     invisible(g)
 }
---
+++ test/visweek2014.R
@@ -0,0 +1,3 @@
+treegraph(business, index=c("NACE1", "NACE2", "NACE3", "NACE4"), show.labels=FALSE, vertex.layout=igraph::layout.auto)
+
+treegraph(business, index=c("NACE1", "NACE2", "NACE3"), show.labels=FALSE, vertex.layout=igraph::layout.auto)
