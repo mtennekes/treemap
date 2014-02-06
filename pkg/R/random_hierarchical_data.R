bf5816ac74400d8f7b59b56593e64b4a6a339cff
Martijn Tennekes <mtennekes@gmaill.com>
2014-02-06 10:21:40 +0100

    random.hierarchical.data finished

--- pkg/R/random_hierarchical_data.R
+++ pkg/R/random_hierarchical_data.R
@@ -1,43 +1,56 @@
-random.hierarchical.data <- function(n=NULL, method="random", ave.number.children=4, children.root=4, depth=3, nodes.per.layer=NULL, children.per.layer=NULL, labels=c("LETTERS", "numbers", "letters"), sep=".") {
+#' Random hierarchical data
+#' 
+#' This function generates random hierarchical data. Experimental.
+#' 
+#' @param n number of leaf nodes. This is a shortcut argument. If specified, the method is set to \code{"random.arcs"} with a nodes.per.layer such that the average number of children per layer is as constant as possible.
+#' @param method one of
+#' \describe{
+#'        \item{\code{"random"}:}{Random tree where for each node, the number of children, is determined by a random poisson generator with \code{lambda=number.children}, until the maximum depth specified by \code{depth} is reached. The number of children of the root node is set to \code{children.root}.}
+#'        \item{\code{"random.arcs"}:}{Random tree where the exact number of nodes in each layer must be speficied by \code{nodes.per.layer}. The arcs between the layers are random, with the restriction that each node is connected.}
+#'        \item{\code{"full.tree"}:}{Each node has exactly \code{number.children} children.}}
+#' @param number.children the number of children. For \code{method="random"} this is the average number of children and for \code{method="full.tree"}, it is the exact number of children. In the latter case, it can also be a vector that specifies the number of children for each layer.
+#' @param children.root number of children of the root node. For \code{method="random"} only.
+#' @param depth depth of the tree. Note that for \code{method="random"}, this depth may not be reached.
+#' @param nodes.per.layer exact number of nodes per layer, that is needed for \code{method="random.arcs"}
+#' @param labels one of \code{"letters"}, \code{"LETTERS"}, \code{"numbers"}, \code{"numbers1"}, \code{"numbers0"}, \code{"hex"}, \code{"bits"}. The label set for \code{"numbers1"} is \code{1:9}, and for \code{"numbers0"} it is \code{0:9}. \code{"numbers"} is equal to \code{"numbers0"}, except that is starts from \code{1}.
+#' @param sep seperator character
+#' @param value.generator function that determine the random values for the leaf nodes
+#' @param value.generator.args list of arguments passed to \code{value.generator}
+#' @example ../examples/random_hierarchical_data.R
+#' @export
+random.hierarchical.data <- function(n=NULL, method="random", number.children=3, children.root=4, depth=3, nodes.per.layer=NULL, labels=c("LETTERS", "numbers", "letters"), sep=".", value.generator=rlnorm, value.generator.args=NULL) {
     
     # check parameters
     if (!missing(n)) {
         nchild <- n^(1/depth)
-        n2 <- n - (n/nchild)
-        nchild <- n2^(1/depth)
-        
-        nodes.per.layer <- nchild^(1:depth)
-        nodes.per.layer <- round(nodes.per.layer * (n/sum(nodes.per.layer)))
-        nodes.per.layer[depth] <- sum(nodes.per.layer[-depth])
-        method <- "fixed.layers"
+        nodes.per.layer <- round(nchild^(1:depth))
+        method <- "random.arcs"
     }
     
-    
-    if (!(method %in% c("random", "fixed.tree", "fixed.layers")))
+    if (!(method %in% c("random", "full.tree", "random.arcs")))
         stop("Incorrect method.")
     if (method=="random") {
-        if (missing(ave.number.children))
+        if (is.null(number.children))
             stop("Required argument ave.number.children not specified.")
-    } else if (method=="fixed.tree") {
-        if (missing(children.per.layer))
-            stop("Required argument children.per.layer not specified.")
-        if (length(children.per.layer)==1)
-            children.per.layer <- rep(children.per.layer, depth) else
-                depth <- length(children.per.layer)
-    } else if (method=="fixed.layers") {
-        if (missing(nodes.per.layer))
+    } else if (method=="full.tree") {
+        if (is.null(number.children))
+            stop("Required argument number.children not specified.")
+        if (length(number.children)==1)
+            number.children <- rep(number.children, depth) else
+                depth <- length(number.children)
+    } else if (method=="random.arcs") {
+        if (is.null(nodes.per.layer))
             stop("Required argument nodes.per.layer not specified.")
         if (length(nodes.per.layer)==1)
             nodes.per.layer <- rep(nodes.per.layer, depth) else
                 depth <- length(nodes.per.layer)
     }
         
-
     # create list with first layer items
     l <- list(as.character(1:switch(method,
                                     random=children.root,
-                                    fixed.tree=children.per.layer,
-                                    fixed.layers=nodes.per.layer[1])))
+                                    full.tree=number.children[1],
+                                    random.arcs=number.children[1])))
     isleaf <- list()
     # fill tree        
     for (d in 2:depth) {
@@ -45,10 +58,10 @@ random.hierarchical.data <- function(n=NULL, method="random", ave.number.childre
         nprev <- length(parents)
 
         x <- switch(method,
-            random = rpois(nprev, lambda=ave.number.children),
-            fixed.tree = rep(children.per.layer, nprev),
-            fixed.layers = rep(1, nprev) + 
-                table(factor(sample(3, children.per.layer[d]-nprev, 
+            random = rpois(nprev, lambda=number.children),
+            full.tree = rep(number.children[d], nprev),
+            random.arcs = rep(1, nprev) + 
+                table(factor(sample.int(nprev, nodes.per.layer[d]-nprev, 
                                     replace=TRUE), levels=1:nprev)))
         l <- c(l, list(unlist(mapply(function(y, z) if (z==0) NULL
                                      else paste(rep(y, z), 1:z, sep=sep),
@@ -56,7 +69,7 @@ random.hierarchical.data <- function(n=NULL, method="random", ave.number.childre
         ## track leafs
         isleaf <- c(isleaf, list(x==0))
     }
-    isleaf <- c(isleaf, rep(TRUE, length(l[[depth]])))
+    isleaf <- c(isleaf, list(rep(TRUE, length(l[[depth]]))))
     
     # remove possible empty layers
     depth <- sum(sapply(l, length)!=0)
@@ -77,23 +90,26 @@ random.hierarchical.data <- function(n=NULL, method="random", ave.number.childre
         
         as.data.frame(rows, stringsAsFactors=FALSE)
     })
+    
+    l <- do.call("rbind", l)
     l <- l[do.call("order", l),]
 
     
     l <- add.labels(l, labels)
     
+    # add values
+    l$x <- do.call("value.generator", args=c(list(nrow(l)), value.generator.args))
+    
     l
 }    
      
 # function to add labels
-
 add.labels <- function(l, labels) {
     # get maxima
     maxx <- sapply(l, function(ll) {
         max(as.integer(gsub(".*[.](.*)", "\\1", ll)), na.rm=TRUE)
     })
-    
-    lvls <- mapply(getSymbols, symbols, maxx)
+    lvls <- mapply(get.symbols, labels, maxx, SIMPLIFY=FALSE)
     
     as.data.frame(lapply(l, function(ll) {
         spl <- lapply(ll, function(lll) strsplit(lll, split=sep, fixed=TRUE)[[1]])
@@ -111,10 +127,7 @@ add.labels <- function(l, labels) {
 
 
 
-#' Function to return vector of sorted symbols
-#' 
-#' @param s one of "letters", "LETTERS", "numbers1", "numbers0", "numbers". The symbol set for numbers1 is 1:9, and for numbers0 it is 0:9. Numbers is equal to numbers0, except that is starts from 1.
-#' @param n number of levels needed
+# Function to return vector of sorted symbols
 get.symbols <- function(s, n) {
     sn <- switch(s, "letters"=26, "LETTERS"=29, "numbers1"=9, "numbers0"=10, "numbers"=10, "hex"=16, "bits"=2)
     symb <- switch(s, "letters"=letters, "LETTERS"=LETTERS, "numbers1"=1:9, "numbers0"=0:9, "numbers"=0:9, "hex"=c(0:9, LETTERS[1:6]), "bits"=0:1)
